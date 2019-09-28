9cdde27f3f288eec561327d23586f066ab607446
Omar Shaikh <oshaikh13@gmail.com>
2019-09-28 13:40:23 -0400

    add hw6

---
+++ hw6/countvowels.asm
@@ -0,0 +1,23 @@
+;;=============================================================
+;;CS 2110 - Fall 2019
+;;Homework 6 - Count the Vowels in a String
+;;=============================================================
+;;Name:
+;;=============================================================
+
+.orig x3000
+
+	;;INSERT CODE AT THIS LOCATION
+
+
+
+HALT
+
+A .fill 0x41    ;; A in ASII
+STRING .fill x4000
+ANSWER .blkw 1
+.end
+
+.orig x4000
+.stringz "TWENTY ONE TEN"
+.end
---
+++ hw6/linkedlist.asm
@@ -0,0 +1,34 @@
+;;=============================================================
+;;CS 2110 - Fall 2019
+;;Homework 6 - Sum the Elements of a Linked List
+;;=============================================================
+;;Name:
+;;=============================================================
+
+.orig x3000
+
+;; YOUR CODE GOES HERE
+
+LL .fill x6000
+ANSWER .blkw 1
+.end
+
+.orig x4000
+.fill x4008
+.fill 5
+.fill x400A
+.fill 2
+.fill x4002
+.fill 9
+.fill x0000 
+.fill 3
+.fill x4004
+.fill 10
+.fill x4006
+.fill 7
+.end
+
+.orig x6000
+.fill x4000
+.fill 6
+.end

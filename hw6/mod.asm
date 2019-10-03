15d47b35019fce703533194638ebe4bec9dd5d74
Omar Shaikh <oshaikh13@gmail.com>
2019-10-02 20:36:11 -0400

    finish mod

--- hw6/mod.asm
+++ hw6/mod.asm
@@ -2,12 +2,21 @@
 ;;CS 2110 - Fall 2019
 ;;Homework 6 - Modulus
 ;;=============================================================
-;;Name:
+;;Name: Omar Shaikh
 ;;=============================================================
 
 .orig x3000
 
-    ;;YOUR CODE GOES HERE
+LD R0, A    ;;YOUR CODE GOES HERE
+LD R1, B
+NOT R1, R1
+ADD	R1,	R1,	1 ;; -B
+
+LOOP
+ADD R0, R0, R1 ;; A = A - B
+BRzp LOOP
+
+ST	R0,	ANSWER
 
 A   .fill x703A
 B   .fill x00FE

1a739cb6ad87e702c84f5931901ebe66fcdc4a71
Omar Shaikh <oshaikh13@gmail.com>
2019-10-03 10:34:56 -0400

    finish LL

--- hw6/linkedlist.asm
+++ hw6/linkedlist.asm
@@ -8,6 +8,22 @@
 .orig x3000
 
 ;; YOUR CODE GOES HERE
+AND R0, R0, 0 ;; sum
+LD R1, LL
+LDR R2, R1, 0 ;; R2 = addr of start
+LDR R3, R1, 1 ;; R3 = length
+
+LOOP
+LDR R5, R2, 1
+ADD R0, R0, R5
+LDR R2, R2, 0 ;; load address of next node 
+ADD R3, R3, -1
+BRz FINISH
+BRnzp LOOP
+
+FINISH
+ST R0, ANSWER
+HALT
 
 LL .fill x6000
 ANSWER .blkw 1
